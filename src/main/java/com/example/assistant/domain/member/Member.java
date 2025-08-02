f65df4e6dd64e43969a6a02cdcd9fad54b16e46b
choi-ys <project.log.062@gmail.com>
2025-08-02 14:20:10 +0900

    feat: 회원 도메인 모델 정의

---
+++ src/main/java/com/example/assistant/domain/member/Member.java
@@ -0,0 +1,37 @@
+package com.example.assistant.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String name;
+
+	private String nickname;
+
+	public Member(String email, String password, String name, String nickname) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.nickname = nickname;
+	}
+}
