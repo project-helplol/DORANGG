d7449950080aff632e98bbf73f12f7060f35bea1
choi-ys <project.log.062@gmail.com>
2025-08-02 16:25:02 +0900

    feat: 회원 가입일 자동 관리를 위한 JpaAudting 설정 및 적용

--- src/main/java/com/example/assistant/AssistantApplication.java
+++ src/main/java/com/example/assistant/AssistantApplication.java
@@ -2,12 +2,14 @@ package com.example.assistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AssistantApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AssistantApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantApplication.class, args);
+	}
 
 }
