8f0f508d4a76aa11ab60c0a1d88832989bfcb45d
choi-ys <project.log.062@gmail.com>
2025-08-02 14:38:35 +0900

    chore: Member Entity와 매핑될 table 이름 명시

--- src/main/java/com/example/assistant/domain/member/Member.java
+++ src/main/java/com/example/assistant/domain/member/Member.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@Table(name = "members")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 	@Id
