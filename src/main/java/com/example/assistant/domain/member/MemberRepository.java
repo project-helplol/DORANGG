9a1b3b97605fed7e4d5403134b3d36cfebea285d
choi-ys <project.log.062@gmail.com>
2025-08-02 15:11:22 +0900

    feat: 로그인 비지니스 로직 구현
    - findByEmail을 이용한 회원 가입 여부 확인
    - passwordEncoder의 matches 메서드를 이용한 비밀번호 일치 여부 검증
    
    TODO
    - 인증이 완료된 사용자에게 Token 발급 부 미 구현

--- src/main/java/com/example/assistant/domain/member/MemberRepository.java
+++ src/main/java/com/example/assistant/domain/member/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.example.assistant.domain.member;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByEmail(String email);
+
+	Optional<Member> findByEmail(String email);
 }
