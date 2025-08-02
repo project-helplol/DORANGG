71b756428f9378b3afe020b30998d0427622fa1c
choi-ys <project.log.062@gmail.com>
2025-08-02 14:38:48 +0900

    fix: 동작하지 않는 코드 주석 처리

--- src/main/java/com/example/assistant/domain/riot/repository/MatchRepository.java
+++ src/main/java/com/example/assistant/domain/riot/repository/MatchRepository.java
@@ -1,14 +1,10 @@
 package com.example.assistant.domain.riot.repository;
 
-import com.example.assistant.domain.riot.entity.Match;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import com.example.assistant.domain.riot.entity.Match;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
-    @Query
-    List<Match> findTopBy20ByRiotIdAndTagline(@Param("riotId") String riotId, @Param("tagline") String tagline, Pageable pageable);
+	// List<Match> findTopBy20ByRiotIdAndTagline(@Param("riotId") String riotId, @Param("tagline") String tagline,
+	// 	Pageable pageable);
 }
